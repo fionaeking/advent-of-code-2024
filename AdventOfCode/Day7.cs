222e80a6b705a7de9f99df23464179d0d01e52ed
Fiona King <FKing@hymans.co.uk>
2024-12-07 09:01:14 +0000

    Add Day 7

--- AdventOfCode/Day7.cs
+++ AdventOfCode/Day7.cs
@@ -4,34 +4,23 @@ public class Day7(string inputFilename) : IDay
 {
     public void Part1()
     {
-        var result = ParseInput(inputFilename).Where(i => IsPossibleEquation(i)).Sum(i => i.TestValue);
+        var result = ParseInput().Where(equation => equation.Inputs.Skip(1)
+                .Aggregate(new List<long> { equation.Inputs[0] }, (current, input) => current.SelectMany(p => new List<long>() { p + input, p * input })
+                    .Where(p => p <= equation.TestValue).ToList()).Contains(equation.TestValue))
+            .Sum(i => i.TestValue);
         Console.WriteLine(result);
     }
 
     public void Part2()
     {
-        var result = ParseInput(inputFilename).Where(i => IsPossibleEquation(i, true)).Sum(i => i.TestValue);
+        var result = ParseInput().Where(equation => equation.Inputs.Skip(1)
+                .Aggregate(new List<long> { equation.Inputs[0] }, (current, input) => current.SelectMany(p => new List<long>() { p + input, p * input, long.Parse(p.ToString() + input) })
+                    .Where(p => p <= equation.TestValue).ToList()).Contains(equation.TestValue))
+            .Sum(i => i.TestValue);
         Console.WriteLine(result);
     }
 
-    private static bool IsPossibleEquation(Equation equation, bool includeConcatenation = false)
-    {
-        var targetValue = equation.TestValue;
-        var possibleResults = new List<long>() { equation.Inputs[0] };
-        foreach (var input in equation.Inputs.Skip(1))
-        {
-            var newValues = possibleResults.Select(p => p + input).ToList();
-            newValues.AddRange(possibleResults.Select(p => p * input));
-            if (includeConcatenation)
-            {
-                newValues.AddRange(possibleResults.Select(p => long.Parse(p.ToString() + input)));
-            }
-            possibleResults = newValues.Where(p => p <= targetValue).ToList();
-        }
-        return possibleResults.Contains(targetValue);
-    }
-
-    private static IEnumerable<Equation> ParseInput(string inputFilename)
+    private IEnumerable<Equation> ParseInput()
     {
         return File.ReadAllLines(inputFilename)
             .Select(i => i.Split(":"))
